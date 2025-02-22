24028660fbe9c310cb7033c18d167c44e99537c7
Rick Slinkman <rw.slinkman@gmail.com>
2025-02-22 22:04:12 +0100

    feat: created Login mode to fetch token based on username/password if no auth token available (#2)
    * feat: Proof of concept - can login using API if username/password available instead of token
    
    * feat: fetch API token if username/password supplied
    
    * restore main location
    
    ---------
    
    Co-authored-by: Rick Slinkman <rick.slinkman@hightechict.nl>

--- main.go
+++ main.go
@@ -1,19 +1,75 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/argoproj/argo-cd/v2/pkg/apiclient"
 	"github.com/argoproj/argo-cd/v2/pkg/apiclient/application"
+	"io"
 	"log"
+	"net/http"
 	"os"
 )
+
+type LoginResponse struct {
+	AuthToken string `json:"token"`
+}
 
 func main() {
 	argoServer, _ := os.LookupEnv("ARGOCD_SERVER")
 	argoApiToken, _ := os.LookupEnv("ARGOCD_API_TOKEN")
 	argoAppName, _ := os.LookupEnv("ARGOCD_APP_NAME")
-	// TODO; define modes LOGIN (username/password) or TOKEN (created using cli)
+	apiUsername, isLoginMode := os.LookupEnv("ARGOCD_API_USERNAME")
+	apiPassword, _ := os.LookupEnv("ARGOCD_API_PASSWORD")
+
+	if isLoginMode {
+		loginPostData := map[string]string{
+			"username": apiUsername,
+			"password": apiPassword,
+		}
+		loginJsonData, err := json.Marshal(loginPostData)
+		if err != nil {
+			fmt.Println("Error encoding JSON:", err)
+			return
+		}
+
+		// Create HTTP POST request
+		argoLoginUrl := fmt.Sprintf("https://%s/api/v1/session", argoServer)
+		req, err := http.NewRequest("POST", argoLoginUrl, bytes.NewBuffer(loginJsonData))
+		if err != nil {
+			fmt.Println("Error creating request:", err)
+			return
+		}
+		req.Header.Set("Content-Type", "application/json")
+
+		// Execute the request
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			fmt.Println("Error making request:", err)
+			return
+		}
+		defer resp.Body.Close()
+
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println("Error reading response:", err)
+			return
+		}
+
+		// Map JSON response to struct
+		var loginResp LoginResponse
+		err = json.Unmarshal(body, &loginResp)
+		if err != nil {
+			fmt.Println("Error decoding JSON:", err)
+			return
+		}
+
+		fmt.Println("Response Body:", loginResp.AuthToken)
+		argoApiToken = loginResp.AuthToken
+	}
 
 	// Create API client options
 	clientOpts := apiclient.ClientOptions{
